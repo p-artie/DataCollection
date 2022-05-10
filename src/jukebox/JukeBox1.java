04978f5c5565e0c4a2f63ca3dee204fd7f3f7f81
p-artie <artiepolanco@gmail.com>
2022-05-10 17:17:12 -0400

    Create jukebox code

---
+++ src/jukebox/JukeBox1.java
@@ -0,0 +1,36 @@
+package jukebox;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class JukeBox1 {
+	ArrayList<String> songList = new ArrayList<String>();
+	public static void main(String[] args) {
+		new JukeBox1().go();
+	}
+	
+	public void go() {
+		getSongs();
+		System.out.println(songList);
+	}
+	
+	void getSongs() {
+		try {
+			File file = new File("SongList.txt");
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				addSong(line);
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	void addSong(String lineToParse) {
+		String[] tokens = lineToParse.split("/");
+		songList.add(tokens[0]);
+	}
+}
